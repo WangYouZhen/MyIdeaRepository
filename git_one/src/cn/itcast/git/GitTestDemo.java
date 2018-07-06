33a9bade2cfb2469128ffad58f20cceeb6dfab2a
WangYouZhen <1820264618@qq.com>
2018-07-06 14:56:04 +0800

    qwqe

--- git_one/src/cn/itcast/git/GitTestDemo.java
+++ git_one/src/cn/itcast/git/GitTestDemo.java
@@ -5,6 +5,10 @@ public class GitTestDemo {
     public static void main(String[] args) {
 
         System.out.println("这是git第一次测试");
+        System.out.println("这是git第一次测试");
+        System.out.println("这是git第一次测试");
+        System.out.println("这是git第一次测试");
+        System.out.println("这是git第一次测试");
 
     }
 }
